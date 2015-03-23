5b20bfc5f11ed00b2f55c35abb42dd83a0cc0af1
Tony <dtonys@gmail.com>
2015-03-23 09:55:01 -0700

    updates to test

--- app.js
+++ app.js
@@ -44,7 +44,15 @@ exports.p = p;
 console.log('<<<<<<<< Test Start >>>>>>>>');
 console.log('browser: '+browser_name+', environment: '+ environment);
 
-var page = '/search';
+var page_urls = [
+  '/search'
+  ,'/topics/cancer'
+  ,'/ask_docs'
+  ,'/user_questions/14082' // question page with lots of data
+  ,'/user_questions/594727' // question page with less data
+  ,'/experts/10000090'
+  ,'/'
+]
 
 // setup
 p.promise = browser
@@ -53,19 +61,20 @@ p.promise = browser
 
 login();
 
-// set the cookie to enable page tracking
-p.promise = p.promise.setCookie({ name: 'page_load_object', value: '{}' });
+var page_times = {};
+// run_page_n_times( '/', 10 )
 
-// run targeted pages n times
-run_page_n_times("/search", 10);
-run_page_n_times("/topics/cancer", 10);
-run_page_n_times("/ask_docs", 10);
+// run targeted page_urls n times
+for( var i in page_urls ){
+  var url = page_urls[i];
+  run_page_n_times( url, 10 );
+}
 
 p.promise = p.promise.eval("docCookies.getItem('page_load_object')", function(err, res){
   console.log(' Results: ');
   console.log( res );
+  printTimes();
   return p.promise;
-  //console.log( JSON.parse(res) );
 });
 
 p.promise = p.promise.deleteCookie( 'page_load_object' );
@@ -81,17 +90,46 @@ function login(){
 }
 
 function run_page_n_times( url, times ){
+  page_times[url] = { times: [], average: null };
   for( var i=0; i<times; i++ ){
     p.promise = p.promise
       .get(url)
       .sleep(1000)
-      .waitForConditionInBrowser("window.performance.timing.domComplete - window.performance.timing.navigationStart > 0", 20000)
-      .eval("window.performance.timing.domComplete - window.performance.timing.navigationStart", function(err, res){
+      .waitForConditionInBrowser("window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart > 0", 20000)
+      .eval("window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart", function(err, res){
         console.log("url : ", url)
         console.log("window.page_load_ms : ", res)
+        page_times[url].times.push( res );
         return p.promise;
       });
   }
+}
+
+// when done, get averages & print times
+function printTimes(){
+  console.log( console.log(new Date()) );
+  // get averages
+  console.log( page_times )
+  for( var url in page_times ){
+    console.log( url + ' average: ' + getAverage( page_times[url].times ) )
+  }
+}
+
+// 
+function getAverage( arr, removeNum ){
+  var removeNum = removeNum || 1;
+  var sum = 0;
+  var avg;
+  
+  // remove top and bottom 2 scores from avg
+  arr.sort( function(a,b){ return a -b  }  )
+  arr.slice( removeNum )
+  arr = arr.slice(0, arr.length-removeNum);
+  for( var i = 0; i < arr.length; i++ ){
+    sum += arr[i];  
+  }
+  avg = Math.floor( sum / arr.length );
+  return avg;
 }
 
 // quit
